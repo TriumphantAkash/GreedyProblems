92e9ceb99cf0004a77e89cd306356d21a00edb19
Akash Chaturvedi <p.akashchaturvedi@gmail.com>
2015-11-09 21:51:31 -0600

    initial files

---
+++ src/greedyStuff/ActivitySelectionProblem.java
@@ -0,0 +1,19 @@
+package greedyStuff;
+/*
+ * Greedy approach is applicable in the condition:
+	1) At every step we can make a choice that looks best at the moment
+	2) We get the optimal solution of the complete problem
+	
+	Note: If a problem can be solved using Greedy algorithm, then that is the best possible solution to that problem
+	However, not all the problems can be solved by the Greedy Algorithm
+	For example, Fractional Knapsack can be solved using Greedy but 0-1 Knapsack can not be solved using Greedy
+*/
+
+public class ActivitySelectionProblem {
+
+	public static void main(String[] args) {
+			
+
+	}
+
+}
